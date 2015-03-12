64c91db15df79831793cfc69e19e405f446d96c7
jakebooyah <xo.myuk@gmail.com>
2015-03-12 12:27:24 +0800

    Remove AndroidSDK compilation files

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Users/jake/dev/cocos2d-x-2.2.6/projects/photon/proj.android/../../../cocos2dx/ccFPSImages.c \
- /Users/jake/dev/cocos2d-x-2.2.6/projects/photon/proj.android/../../../cocos2dx/ccFPSImages.h
-
-/Users/jake/dev/cocos2d-x-2.2.6/projects/photon/proj.android/../../../cocos2dx/ccFPSImages.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /Users/jake/dev/cocos2d-x-2.2.6/projects/photon/proj.android/../../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /Users/jake/dev/cocos2d-x-2.2.6/projects/photon/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/Users/jake/dev/cocos2d-x-2.2.6/projects/photon/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
