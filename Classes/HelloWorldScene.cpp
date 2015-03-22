a42111894e187c7e8089664ef1309b5e175541b7
jakebooyah <xo.myuk@gmail.com>
2015-03-23 02:23:33 +0800

    Updated bg.png

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -459,7 +459,7 @@ void HelloWorld::update(float delta)
     }
     
     //if all player joined
-    if (true /*Player2Joined && Player3Joined && Player4Joined*/)
+    if (Player2Joined /*&& Player3Joined && Player4Joined*/)
     {
         //if from Host
         if (networkLogic->playerNr == 1)
