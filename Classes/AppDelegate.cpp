31b7d1a919ce020a20142a9d0aa933c21182b350
jakebooyah <xo.myuk@gmail.com>
2015-04-23 16:28:41 +0800

    Removed fps debug

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -23,7 +23,7 @@ bool AppDelegate::applicationDidFinishLaunching()
     CCEGLView::sharedOpenGLView()->setDesignResolutionSize(2730, 1536, kResolutionFixedHeight);
 	
     // turn on display FPS
-    pDirector->setDisplayStats(true);
+//    pDirector->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
     pDirector->setAnimationInterval(1.0 / 60);
